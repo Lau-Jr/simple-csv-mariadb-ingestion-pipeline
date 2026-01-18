b9e4192d27d189c311dca4a93a13aeb1bbd0652b
Lau-Jr <laurentdeusdedith74@gmail.com>
2026-01-18 16:03:11 +0300

    ingested to local mariadb

---
+++ ingestion_pipeline.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from sqlalchemy import create_engine
+
+# CSV file path
+csv_file = "data.csv"
+
+# Read CSV
+df = pd.read_csv(csv_file)
+
+# Database connection
+user = "root"
+password = "root"
+host = "localhost"
+port = 3306
+database = "testdb"
+
+engine = create_engine(f"mariadb+mariadbconnector://{user}:{password}@{host}:{port}/{database}")
+
+# Load data into table
+df.to_sql('users', con=engine, if_exists='replace', index=False)
+
+print("Data ingested successfully!")
