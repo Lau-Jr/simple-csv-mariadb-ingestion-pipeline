421cfdee1585a1569539bab677b5e28f655e5e66
Lau-Jr <laurentdeusdedith74@gmail.com>
2026-01-18 16:17:04 +0300

    changed port to one on docker

--- ingestion_pipeline.py
+++ ingestion_pipeline.py
@@ -11,7 +11,7 @@ df = pd.read_csv(csv_file)
 user = "root"
 password = "root"
 host = "localhost"
-port = 3306
+port = 3307
 database = "testdb"
 
 engine = create_engine(f"mariadb+mariadbconnector://{user}:{password}@{host}:{port}/{database}")
